e89802aac4dff3367703098b789b03cb9cd161c8
shpali4 <shamardinvs@yandex.ru>
2022-12-21 00:49:38 +0700

    CarService: убрана аннотация @Component

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,13 +1,11 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import web.models.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
 @Service
 public class CarService {
     public List<Car> showListOfCars(List<Car> cars, int count) {
